808e512b3937025c7df7ff0084d37b291c919885
dovazencot <dov.azencot@gmail.com>
2025-03-11 12:51:50 +0100

    update using webpacke

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,28 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    turbo: {
+      resolveAlias: {
+        '@nimiq/core': '@nimiq/core/bundler/index.js',
+      },
+    },
+  },
+  webpack: (config, { isServer }) => {
+    // Enable WebAssembly
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+    
+    // Configure module rules for WebAssembly
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+    
+    return config;
+  },
 };
 
 export default nextConfig;
